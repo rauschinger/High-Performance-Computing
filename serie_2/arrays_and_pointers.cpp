43625f343ec11002780102cfe11eb1769b041d8b
Niklas Rausch <niklas.rausch@uni-jena.de>
2022-04-26 23:59:54 +0200

    edits

--- serie_2/arrays_and_pointers.cpp
+++ serie_2/arrays_and_pointers.cpp
@@ -7,6 +7,15 @@ int main() {
 
     int * l_my_var = (int*) malloc ( 1500 * sizeof(int));
 
+    unsigned char l_my_char = 5;
+    std::cout << "sizeof( l_my_char ): " << sizeof(l_my_char) << std::endl;
+
+
+    //l_my_var[1000] = 5;
+    *(l_my_var+1000) = 4;
+    std::cout << l_my_var[1000] << std::endl;
+
+
     free (l_my_var);
 
     return EXIT_SUCCESS;
