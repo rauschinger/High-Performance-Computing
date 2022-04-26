6cd8f14b09a4f6aee0361a1e3dadd3055584f882
Niklas Rausch <niklas.rausch@uni-jena.de>
2022-04-26 23:40:34 +0200

    edits

--- serie_2/arrays_and_pointers.cpp
+++ serie_2/arrays_and_pointers.cpp
@@ -1,6 +1,13 @@
 #include <cstdlib>
+#include <iostream>
 
 int main() {
+
+    std::cout << "hello world!" << std::endl;
+
+    int * l_my_var = (int*) malloc ( 1500 * sizeof(int));
+
+    free (l_my_var);
 
     return EXIT_SUCCESS;
 
